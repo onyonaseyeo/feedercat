8dfa403a67cb008f2fa5b4436197b3f420d7db44
onyonaseyeo <123248318+onyonaseyeo@users.noreply.github.com>
2023-07-05 21:37:47 +0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from PIL import Image
+import paho.mqtt.client as mqtt
+
+
+image = Image.open('doraemon.jpg')
+st.image(image, caption='Automatic Cat Feeder')
+
+st.title ("Automatic Pet Feeder")
+st.subheader ("Sisa Pakan")
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("petfeeder/ura$^cp22/jarak")
+def on_disconnect(client, userdata, flags, rc):
+    st.success("Disconect")
+def get_mqtt_client():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_publish = on_publish
+    return client
+def on_publish(client, userdata, mid):
+    print("mid: " + str(mid))
+stok = st.empty()
+def on_message(client, userdata, msg):
+    stok.metric("Sisa pakan", msg.payload.decode("utf-8"))
+
+broker = "mqtt-dashboard.com"
+port = 1883
+subscribeTopic = "petfeeder/ura$^cp22/jarak"
+client = get_mqtt_client()
+client.on_publish = on_publish
+client.connect(broker, int(port), 60)
+
+while True:
+    client.loop()
