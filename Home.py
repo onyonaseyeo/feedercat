bab0e41a7ff949d64bade51e4667a0888cf19c70
onyonaseyeo <123248318+onyonaseyeo@users.noreply.github.com>
2023-07-09 21:31:03 +0700

    Home.py

--- Home.py
+++ Home.py
@@ -1,19 +1,18 @@
 import streamlit as st
+import sqlite3
 from secrets import choice
 import paho.mqtt.client as mqtt
 
-cnx = st.experimental_connection('mysql', type='sql')
-
+cnx = sqlite3.connect('data.db')
 cursor = cnx.cursor()
 
  # MQTT
 broker = "mqtt-dashboard.com"
 port = 1883
-topic = "petfeeder/ura$^cp22/beratt"
 publishTopic = "petfeeder/ura$^cp22/servo/"
 subscribeTopic = "petfeeder/ura$^cp22/jarak"
 
-menu = ["Home", "Data", "Daftar", "Manual"]
+menu = ["Home", "Manual"]
 choice = st.sidebar.selectbox("Menu", menu)
 
 image = Image.open('doraemon.jpg')
@@ -42,31 +41,31 @@ client = get_mqtt_client()
 client.on_publish = on_publish
 client.connect(broker, int(port), 60)
 
-if choice == "Data":
-    st.header("Data Kucing")
-    # Baca data dari tabel ke dataframe
-    df = cnx.query('SELECT id, nama, umur, berat FROM data', ttl=600)
+# if choice == "Data":
+#     st.header("Data Kucing")
+#     # Baca data dari tabel ke dataframe
+#     df = cnx.query('SELECT id, nama, umur, berat FROM data', ttl=600)
 
-    # Memformat data sebelum menampilkan
-    df['berat'] = df['berat'].apply(lambda x: '{:.1f}'.format(x))
-    st.dataframe(df)
+#     # Memformat data sebelum menampilkan
+#     df['berat'] = df['berat'].apply(lambda x: '{:.1f}'.format(x))
+#     st.dataframe(df)
 
-elif choice == "Daftar":    
-    with st.form(key="my_form"):
-        nama = st.text_input('Nama')
-        berat = st.number_input('Berat (gram)', max_value=5000, min_value=500)
-        umur = st.number_input('Umur (bulan)', max_value=50, min_value=0)
-        daftar = st.form_submit_button (label="Daftar")
-        if daftar:
-            data = {"namakucing": nama, "berat": berat, "umur": umur}
-            if nama and umur:
-                insert_query = "INSERT INTO datakucing (nama, berat, umur) VALUES (%s, %s, %s)"
-                insert_data = (nama, berat, umur)
-                cursor.execute(insert_query, insert_data)
-                cnx.commit()
-                st.success(f"{nama} dengan Berat {berat} dan Umur {umur} Bulan Telah Terdaftar")
-            else :
-                st.error('Silahkan Isi Data')
+# elif choice == "Daftar":    
+#     with st.form(key="my_form"):
+#         nama = st.text_input('Nama')
+#         berat = st.number_input('Berat (gram)', max_value=5000, min_value=500)
+#         umur = st.number_input('Umur (bulan)', max_value=50, min_value=0)
+#         daftar = st.form_submit_button (label="Daftar")
+#         if daftar:
+#             data = {"namakucing": nama, "berat": berat, "umur": umur}
+#             if nama and umur:
+#                 insert_query = "INSERT INTO datakucing (nama, berat, umur) VALUES (%s, %s, %s)"
+#                 insert_data = (nama, berat, umur)
+#                 cursor.execute(insert_query, insert_data)
+#                 cnx.commit()
+#                 st.success(f"{nama} dengan Berat {berat} dan Umur {umur} Bulan Telah Terdaftar")
+#             else :
+#                 st.error('Silahkan Isi Data')
 
 elif choice == "Manual":
     if st.button("Beri Pakan"):
