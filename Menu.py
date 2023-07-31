77b4fc4f0cadda1773bf546cd776e0df94560b5d
onyonaseyeo <123248318+onyonaseyeo@users.noreply.github.com>
2023-07-31 20:54:54 +0700

    Menu.py

--- Menu.py
+++ Menu.py
@@ -2,9 +2,9 @@ import streamlit as st
 from paho.mqtt import client as mqtt
 
 st.title ("Pakan Kucing")
-    # MQTT
+    
 def on_connect(client, userdata, flags, rc):
-    client.subscribe("petfeeder/ura$^cp22/jarak")
+    client.subscribe("petfeeder/ura$^cp22/stokpakan")
 def on_disconnect(client, userdata, flags, rc):
     st.write("Disconect")
 def get_mqtt_client():
@@ -18,11 +18,12 @@ def on_publish(client, userdata, mid):
 def on_message(client, userdata, msg):
     st.write(msg.topic+ "::" + str(msg.payload))
 
+# MQTT
 broker = "mqtt-dashboard.com"
 port = 1883
-topic = "petfeeder/ura$^cp22/servo"
 publishTopic = "petfeeder/ura$^cp22/servo"
-subscribeTopic = "petfeeder/ura$^cp22/jarak"
+subscribeTopic = "petfeeder/ura$^cp22/stokpakan"
+subscribeTopic1 = "petfeeder/ura$^cp22/pakan"
 
 client = get_mqtt_client()
 client.on_publish = on_publish
@@ -31,14 +32,12 @@ client.connect(broker, int(port), 60)
 if st.button("Beri Pakan"):
     client.publish(publishTopic, "open")
 
-
-
 st.subheader ("Sisa Pakan")
 if st.button("Stok Pakan"):
     def on_connect(client, userdata, flags, rc):
-        client.subscribe("petfeeder/ura$^cp22/jarak")
+        client.subscribe("petfeeder/ura$^cp22/stokpakan")
     def convert_to_percentage(distance):
-        # Lakukan perhitungan konversi sesuai dengan skala jarak Anda
+        # Perhitungan konversi 
         #min_distance = 1  # Jarak minimum
         max_distance = 11  # Jarak maksimum
         percentage = (max_distance - distance) * 10
@@ -51,6 +50,27 @@ if st.button("Stok Pakan"):
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
+    client.connect("mqtt-dashboard.com", 1883, 60)
+
+    while True:
+        client.loop()
+
+if st.button("Status Pakan"):
+    def on_connect1(client, userdata, flags, rc):
+        client.subscribe("petfeeder/ura$^cp22/pakan")
+    def convert(distance1):
+        if distance1 <= 20:
+            st.success("Pakan Tersedia")
+        else :
+            st.error("Pakan Tidak Tersedia")
+    def on_message1(client, userdata, msg):
+        distance1 = int(msg.payload.decode("utf-8"))
+        status = convert (distance1)
+        # st.write("Status: ","Pakan Tersedia")
+
+    client = mqtt.Client()
+    client.on_connect = on_connect1
+    client.on_message = on_message1
     client.connect("mqtt-dashboard.com", 1883, 60)
 
     while True:
